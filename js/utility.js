32d143e60ec606ab58baf06c2ab193eda8741333
dvranga <dvranga@users.noreply.github.com>
2020-12-11 03:14:47 +0530

    [ranganath]updated:Finally make sure the employee payroll app works like before

--- js/utility.js
+++ js/utility.js
@@ -5,7 +5,7 @@ const stringyfyDate = (date) => {
 }
 
 const update = (node) => {
-    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
     if(!empPayrollData) return;
     localStorage.setItem('editEmp', JSON.stringify(empPayrollData))
     window.location.replace(site_properties.add_emp_payroll_page);
